6cf9fb39b0a158e07476e67c8a6f539ebe46d49b
wixxm <419691642@qq.com>
2025-07-07 20:35:37 +0800

    🚀 Sync r4s_build_script - 2025-07-07 20:35:37

--- openwrt/patch/openwrt-6.x/modules/hwmon.mk
+++ openwrt/patch/openwrt-6.x/modules/hwmon.mk
@@ -362,7 +362,7 @@ define KernelPackage/hwmon-lm92
   KCONFIG:=CONFIG_SENSORS_LM92
   FILES:=$(LINUX_DIR)/drivers/hwmon/lm92.ko
   AUTOLOAD:=$(call AutoProbe,lm92)
-  $(call AddDepends/hwmon,+kmod-i2c-core)
+  $(call AddDepends/hwmon,+kmod-i2c-core +kmod-regmap-core)
 endef
 
 define KernelPackage/hwmon-lm92/description
@@ -548,7 +548,7 @@ define KernelPackage/hwmon-sch5627
 	$(LINUX_DIR)/drivers/hwmon/sch5627.ko \
 	$(LINUX_DIR)/drivers/hwmon/sch56xx-common.ko
   AUTOLOAD:=$(call AutoProbe,sch5627)
-  $(call AddDepends/hwmon,+kmod-i2c-core)
+  $(call AddDepends/hwmon,+kmod-i2c-core +kmod-regmap-core)
 endef
 
 define KernelPackage/hwmon-sch5627/description
